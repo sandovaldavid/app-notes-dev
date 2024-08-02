560f6145786bed5644f51e7ece1c287292c743ae
sandovaldavid <sandovaldavid2201@gmail.com>
2024-08-02 01:57:00 -0500

    Formmat Code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 
-const notesRoutes= require('./routes/notes.routes');
+const notesRoutes = require("./routes/notes.routes");
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 
 app.use(notesRoutes);
 
 app.listen(3000);
-console.log('Server started on port 3000');
+console.log("Server started on port 3000");
