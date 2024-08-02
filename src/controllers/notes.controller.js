600ad592cbe58bff279493443c943d86b3cf1463
sandovaldavid <sandovaldavid2201@gmail.com>
2024-08-02 01:58:00 -0500

    Complete metthod 'getAllNotes' and 'createNote'

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -1,5 +1,12 @@
+const pool = require("../db");
+
 const getAllNotes = async (req, res) => {
-	res.send("Retrieving all notes");
+	try {
+		const allNotes = await pool.query("SELECT * FROM notes");
+		res.json(allNotes.rows);
+	} catch (error) {
+		console.log(error.message);
+	}
 };
 
 const getNote = async (req, res) => {
@@ -7,7 +14,16 @@ const getNote = async (req, res) => {
 };
 
 const createNote = async (req, res) => {
-	res.send("creating a new note");
+	const { title, content } = req.body;
+	try {
+		const result = await pool.query(
+			"INSERT INTO notes (title, content) VALUES ($1, $2) RETURNING *",
+			[title, content]
+		);
+		res.json(result.rows[0]);
+	} catch (error) {
+		res.json({ error: error.message });
+	}
 };
 
 const deleteNote = async (req, res) => {
