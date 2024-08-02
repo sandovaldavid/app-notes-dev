21b58e9aca332097e25162551ffd60c7a8b8fea6
sandovaldavid <sandovaldavid2201@gmail.com>
2024-08-02 14:36:19 -0500

    Add: cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 
 const notesRoutes = require("./routes/notes.routes");
 
 const app = express();
 
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
