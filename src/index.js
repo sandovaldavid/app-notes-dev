10c139694e891d756fcd62ccf9463cd142d01b8e
sandovaldavid <sandovaldavid2201@gmail.com>
2024-08-01 23:57:09 -0500

    Add nodemon module for better development.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+app.use(morgan('dev'));
 
 app.listen(3000);
 console.log('Server started on port 3000');
