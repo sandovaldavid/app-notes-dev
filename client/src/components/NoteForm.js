4d8edd7347dff18a08c594e3552565f6db3e2de8
sandovaldavid <sandovaldavid2201@gmail.com>
2024-08-04 18:22:06 -0500

    delete files

--- client/src/components/NoteForm.js
+++
@@ -1,6 +0,0 @@
-
-export default function NoteForm() {
-  return (
-    <div>NoteForm</div>
-  )
-}
--- client/src/components/NoteList.js
+++
@@ -1,7 +0,0 @@
-
-
-export default function NoteList() {
-  return (
-    <div>NoteList</div>
-  )
-}
