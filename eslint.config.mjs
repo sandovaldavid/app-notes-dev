671398f3c41ace851414e1b8567ae9206c4949c4
sandovaldavid <sandovaldavid2201@gmail.com>
2024-08-31 09:15:35 -0500

    chore: Add linting configuration and scripts

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+export default [
+	{ files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+	{ languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+	pluginJs.configs.recommended,
+	{
+		rules: {
+			"no-multiple-empty-lines": ["error", { max: 1, maxEOF: 0 }],
+      "eol-last": ["error", "never"]
+		},
+	},
+];
