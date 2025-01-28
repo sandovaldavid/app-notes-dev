03e359fe4af8b0b85497e3922af749da9ab104c5
sandovaldavid <sandovaldavid2201@gmail.com>
2025-01-27 22:09:29 -0500

    ♻️ refactor project, split backend y frontend

--- database/db.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE db_webnotes
-
-CREATE TABLE notes (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    content TEXT NOT NULL,
-);
--- src/index.js
+++ src/index.js
@@ -1,20 +1,17 @@
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const notesRoutes = require("./routes/notes.routes");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
 
-const app = express();
-
-app.use(cors());
-app.use(morgan("dev"));
-app.use(express.json());
-
-app.use(notesRoutes);
-
-app.use((err, req, res) => {
-	return res.json({ message: err.message });
-});
-
-app.listen(3000);
-console.log("Server started on port 3000"); 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
